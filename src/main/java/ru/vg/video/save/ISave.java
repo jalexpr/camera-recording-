9a6778f7201afb17a340b4c14ae6603513bcfb9d
jalexpr <alex.porechny@mail.ru>
2019-05-13 21:59:47 +0300

    add formatNowTime

--- src/main/java/ru/vg/video/save/ISave.java
+++ src/main/java/ru/vg/video/save/ISave.java
@@ -4,7 +4,7 @@ import java.awt.image.BufferedImage;
 import java.text.SimpleDateFormat;
 
 public interface ISave {
-    public static SimpleDateFormat formatNowTime = new SimpleDateFormat("HH_mm");
+    public static SimpleDateFormat formatNowTime = new SimpleDateFormat("HH_mm_ss");
     public static SimpleDateFormat formatNowTimeFull = new SimpleDateFormat("HH_mm_ss_SS");
 
     public void writerImage(BufferedImage bgrScreen);
