0b85d2f0c3ef0b80934e5c656f3e1b107d9c00f3
jalexpr <alex.porechny@mail.ru>
2020-01-26 20:09:39 +0200

    add sleep

--- src/main/java/ru/vg/SleepController.java
+++ src/main/java/ru/vg/SleepController.java
@@ -2,17 +2,31 @@ package ru.vg;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Map;
+import java.io.InputStreamReader;
 
 @RestController()
 @RequestMapping("/api")
 public class SleepController {
     @RequestMapping(value = "sleep", method = RequestMethod.GET)
-    public void sleep(@RequestParam Map<String, String> params) throws IOException {
-        Process p = Runtime.getRuntime().exec("cmd /c start \"\" sleep.bat");
+    public String sleep() throws IOException {
+        return call("sleep.bat");
+    }
+
+    @RequestMapping(value = "restart", method = RequestMethod.GET)
+    public String  restart() throws IOException {
+        return call("restart.bat");
+    }
+
+    private String call(String bat) throws IOException {
+        Process p = Runtime.getRuntime().exec("cmd /c start \"\" " + bat);
+        if (p.getErrorStream() != null) {
+            return new BufferedReader(new InputStreamReader(p.getErrorStream(), "cp866")).readLine();
+        } else {
+            return "ok";
+        }
     }
 }
